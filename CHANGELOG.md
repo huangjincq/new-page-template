6699eb3c0c97aac14032dafa6275290c5043ca52
huangjin2 <huangjin2@webull.com>
2024-11-07 09:46:52 +0800

    初始化

---
+++ CHANGELOG.md
@@ -0,0 +1,22 @@
+# Change Log
+
+All notable changes to the "Omni BO" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [Unreleased]
+
+## [0.0.1] - 2024-05-14
+
+### Added
+
+- 完成插件创建模版文件夹核心功能
+
+## [1.0.0] - 2024-11-6
+
+### Added
+
+- 用 `Webview` 的形式提交配置数据
+- 根据配置动态生成模版文件内容
+- 自动生成并修改路由文件
+- 增加识别图片生成表格列
---
+++ dist/templates/index.tsx
@@ -0,0 +1,171 @@
+import React, { useEffect, useMemo, useRef } from 'react'
+import OfficeProTable from '@library/officeComponents/OfficeProTable'
+import { ActionType } from '@ant-design/pro-components'
+import moment from 'moment'
+import { useColumns } from './columns'
+import api from './api'
+import { useSetState } from 'ahooks'
+/* feature_edit_start */
+import EditModal from './components/EditModal'
+/* feature_edit_end */
+/* feature_detail_start */
+import DetailModal from './components/DetailModal'
+/* feature_detail_end */
+import { OfficeButton } from '@library/officeComponents'
+import { PlusOutlined } from '@ant-design/icons'
+import { SuccessMsg } from '@library/hooks/global'
+import { Modal, Space } from 'antd'
+
+export default function Template() {
+  const actionRef = useRef<ActionType>()
+
+  const [
+    {
+      selectedRow,
+      /* feature_batch_start */
+      selectedRows,
+      /* feature_batch_end */
+      /* feature_detail_start */
+      showDetailModal,
+      /* feature_detail_end */
+      /* feature_edit_start */
+      showEditModal
+      /* feature_edit_end */
+    },
+    setState
+  ] = useSetState({
+    selectedRow: null,
+    /* feature_batch_start */
+    selectedRows: [],
+    /* feature_batch_end */
+    /* feature_detail_start */
+    showDetailModal: false,
+    /* feature_detail_end */
+    /* feature_edit_start */
+    showEditModal: false
+    /* feature_edit_end */
+  })
+
+  /* feature_batch_start */
+  const selectedRowKeys = useMemo(() => selectedRows.map((v) => v.id), [selectedRows])
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: (selectedRowKeys: string[], selectedRows) => {
+      setState({ selectedRows })
+    }
+    // getCheckboxProps: (record) => ({
+    //   disabled: record.status === 1,
+    // }),
+  }
+  /* feature_batch_end */
+
+  /* feature_edit_start */
+  const handleEditModal = (selectedRow = null) => {
+    setState({ showEditModal: true, selectedRow })
+  }
+  /* feature_edit_end */
+
+  /* feature_detail_start */
+  const handleDetailModal = (selectedRow) => {
+    setState({ showDetailModal: true, selectedRow })
+  }
+  /* feature_detail_end */
+
+  const handleSubmit = async (selectedRow) => {
+    // TODO do something
+    SuccessMsg()
+    actionRef.current.reload()
+  }
+
+  /* feature_delete_start */
+  const handleDelete = async (selectedRow = null) => {
+    Modal.confirm({
+      content: 'Are you sure to delete?',
+      onOk: async () => {
+        await api.delete({ ids: [selectedRow?.id] })
+        SuccessMsg()
+        actionRef.current.reload()
+      }
+    })
+  }
+  /* feature_delete_end */
+
+  const [formColumns, tableColumns] = useColumns({
+    /* feature_edit_start */
+    handleEditModal,
+    /* feature_edit_end */
+    /* feature_detail_start */
+    handleDetailModal,
+    /* feature_detail_end */
+    /* feature_delete_start */
+    handleDelete,
+    /* feature_delete_end */
+    handleSubmit
+  })
+
+  const onClose = (refresh?: boolean) => {
+    setState({
+      selectedRow: null,
+      /* feature_detail_start */
+      showDetailModal: false,
+      /* feature_detail_end */
+      /* feature_edit_start */
+      showEditModal: false
+      /* feature_edit_end */
+    })
+    refresh && actionRef.current.reload()
+  }
+
+  return (
+    <>
+      <OfficeProTable
+        actionRef={actionRef}
+        formColumns={formColumns}
+        tableColumns={tableColumns}
+        request={api.list}
+        /* feature_batch_start */
+        rowSelection={rowSelection}
+        onDataSourceChange={() => setState({ selectedRows: [] })}
+        /* feature_batch_end */
+        /* feature_export_start */
+        frontEndExport={{
+          exportFileName: `ODK_PositionActivityListing_${moment().format('yyyy-MM-DD')}`,
+          permissionUrl: '' // TODO set permission url
+        }}
+        /* feature_export_end */
+        toolbar={{
+          title: (
+            <Space align="end" size="middle">
+              /* feature_edit_start */
+              <OfficeButton
+                url="" // TODO set permission url
+                onClick={() => handleEditModal()}
+                type="primary"
+                icon={<PlusOutlined />}
+              >
+                Add
+              </OfficeButton>
+              /* feature_edit_end */ /* feature_batch_start */ /* feature_delete_start */
+              <OfficeButton
+                url="" // TODO set permission url
+                type="primary"
+                danger
+                disabled={!selectedRows.length}
+                onClick={handleDelete}
+              >
+                Batch Delete
+              </OfficeButton>
+              /* feature_delete_end */ /* feature_batch_end */
+            </Space>
+          )
+        }}
+      />
+      {/* feature_edit_start */}
+      {showEditModal && <EditModal selectedRow={selectedRow} onClose={onClose} />}
+      {/* feature_edit_end */}
+      {/* feature_detail_start */}
+      {showDetailModal && <DetailModal selectedRow={selectedRow} onClose={onClose} />}
+      {/* feature_detail_end */}
+    </>
+  )
+}
